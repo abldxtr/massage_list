3a71f6c917935d3fb0885ddf3d421eb14c36ee55
abldxtr <110703641+abldxtr@users.noreply.github.com>
2024-06-03 06:40:36 +0000

    fix some issue

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
+    <main className="flex min-h-screen overflow-hidden flex-col items-center justify-between">
       <MessageList />
     </main>
   );
